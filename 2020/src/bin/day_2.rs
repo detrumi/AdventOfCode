f157015fac707c88ff67f1628f95eaf1654f16c6
Wilco Kusee <wilcokusee@gmail.com>
2020-12-02 10:53:54 +0100

    Slightly fancier day 2

--- 2020/src/bin/day_2.rs
+++ 2020/src/bin/day_2.rs
@@ -1,44 +1,31 @@
 const INPUT: &str = include_str!("../../input/day_2.txt");
 
-fn part1() -> usize {
-    let mut result = 0;
-    for line in INPUT.lines() {
+fn parse<'a>() -> impl Iterator<Item = (usize, usize, char, &'a str)> {
+    INPUT.lines().map(|line| {
         let words: Vec<_> = line.split_ascii_whitespace().collect();
         let lengths: Vec<_> = words[0]
             .split('-')
             .map(|n| n.parse::<usize>().unwrap())
             .collect();
-        let name = words[1].trim_end_matches(':');
+        let value = words[1].trim_end_matches(':').chars().next().unwrap();
         let pass = words[2];
+        (lengths[0], lengths[1], value, pass)
+    })
+}
 
-        let count = pass
-            .chars()
-            .filter(|&c| c == name.chars().next().unwrap())
-            .count();
-        if count >= lengths[0] && count <= lengths[1] {
-            result += 1;
-        }
-    }
-
-    result
+fn part1() -> usize {
+    parse()
+        .filter(|(a, b, value, pass)| (a..=b).contains(&&pass.matches(*value).count()))
+        .count()
 }
 
 fn part2() -> usize {
-    let mut result = 0;
-    for line in INPUT.lines() {
-        let words: Vec<_> = line.split_ascii_whitespace().collect();
-        let lengths: Vec<_> = words[0]
-            .split('-')
-            .map(|n| n.parse::<usize>().unwrap())
-            .collect();
-        let name = words[1].trim_end_matches(':').chars().next().unwrap();
-        let pass: Vec<_> = words[2].chars().collect();
-
-        if (pass[lengths[0] - 1] == name) != (pass[lengths[1] - 1] == name) {
-            result += 1;
-        }
-    }
-    result
+    parse()
+        .filter(|(a, b, value, pass)| {
+            let chars: Vec<_> = pass.chars().collect();
+            (chars[a - 1] == *value) != (chars[b - 1] == *value)
+        })
+        .count()
 }
 
 fn main() {
