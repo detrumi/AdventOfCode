a66b38b63656ece898b393e10ee591f635496700
Wilco Kusee <wilcokusee@gmail.com>
2020-12-03 06:35:52 +0100

    Further simplify 2020 day 3

--- 2020/src/bin/day_3.rs
+++ 2020/src/bin/day_3.rs
@@ -1,42 +1,28 @@
 const INPUT: &str = include_str!("../../input/day_3.txt");
 
-fn parse() -> (Vec<bool>, (usize, usize)) {
-    let mut trees = vec![];
-
-    let lines: Vec<_> = INPUT.lines().collect();
-    let size = (lines[0].len(), lines.len() - 1);
-    for line in lines {
-        for c in line.chars() {
-            trees.push(if c == '#' { true } else { false });
-        }
-    }
-    (trees, size)
+fn parse() -> Vec<Vec<bool>> {
+    INPUT
+        .lines()
+        .map(|line| line.chars().map(|c| c == '#').collect())
+        .collect()
 }
 
-fn solve(trees: &[bool], size: (usize, usize), slope: (usize, usize)) -> usize {
-    let mut count = 0;
-    let mut pos = (0, 0);
-    loop {
-        if trees[size.0 * pos.1 + pos.0] {
-            count += 1;
-        }
-        pos = ((pos.0 + slope.0) % size.0, pos.1 + slope.1);
-        if pos.1 > size.1 {
-            return count;
-        }
-    }
+fn solve(trees: &[Vec<bool>], slope: (usize, usize)) -> usize {
+    (0..)
+        .take_while(|n| n * slope.1 < trees.len())
+        .filter(|n| trees[n * slope.1][(n * slope.0) % trees[0].len()])
+        .count()
 }
 
 fn part1() -> usize {
-    let (trees, size) = parse();
-    solve(&trees, size, (3, 1))
+    solve(&parse(), (3, 1))
 }
 
 fn part2() -> usize {
-    let (trees, size) = parse();
+    let trees = parse();
     [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
         .iter()
-        .map(|&slope| solve(&trees, size, slope))
+        .map(|&slope| solve(&trees, slope))
         .product()
 }
 
