7e6f5bd3899eac01741d6daa63dcdd5f7897198d
Wilco Kusee <wilcokusee@gmail.com>
2020-12-05 12:42:25 +0100

    Clean up solution

--- 2020/src/bin/day_5.rs
+++ 2020/src/bin/day_5.rs
@@ -3,49 +3,41 @@ const INPUT: &str = include_str!("../../input/day_5.txt");
 fn part1() -> usize {
     let mut best = 0;
     for line in INPUT.lines() {
-        let mut r = (0, 127);
-        let mut c = (0, 7);
-        for curr in line.chars() {
-            match curr {
-                'F' => r = (r.0, r.1 - (r.1 - r.0 + 1) / 2),
-                'B' => r = (r.0 + (r.1 - r.0 + 1) / 2, r.1),
-                'L' => c = (c.0, c.1 - (c.1 - c.0 + 1) / 2),
-                'R' => c = (c.0 + (c.1 - c.0 + 1) / 2, c.1),
-                _ => panic!(),
-            }
-        }
-        best = best.max(8 * r.0 + c.1);
+        let mut r = 0..128;
+        let mut c = 0..8;
+        line.chars().for_each(|ch| match ch {
+            'F' => r = r.start..r.end - (r.end - r.start + 1) / 2,
+            'B' => r = r.start + (r.end - r.start + 1) / 2..r.end,
+            'L' => c = c.start..c.end - (c.end - c.start + 1) / 2,
+            'R' => c = c.start + (c.end - c.start + 1) / 2..c.end,
+            _ => panic!(),
+        });
+        best = best.max(8 * r.start + c.end - 1);
     }
     best
 }
 
 fn part2() -> usize {
-    let mut best = 0;
     let mut found = vec![];
     for line in INPUT.lines() {
-        let mut r = (0, 127);
-        let mut c = (0, 7);
-        for curr in line.chars() {
-            match curr {
-                'F' => r = (r.0, r.1 - (r.1 - r.0 + 1) / 2),
-                'B' => r = (r.0 + (r.1 - r.0 + 1) / 2, r.1),
-                'L' => c = (c.0, c.1 - (c.1 - c.0 + 1) / 2),
-                'R' => c = (c.0 + (c.1 - c.0 + 1) / 2, c.1),
-                _ => panic!(),
-            }
-        }
-        found.push(8 * r.0 + c.1);
-        best = best.max(8 * r.0 + c.1);
+        let mut r = 0..128;
+        let mut c = 0..8;
+        line.chars().for_each(|ch| match ch {
+            'F' => r = r.start..r.end - (r.end - r.start + 1) / 2,
+            'B' => r = r.start + (r.end - r.start + 1) / 2..r.end,
+            'L' => c = c.start..c.end - (c.end - c.start + 1) / 2,
+            'R' => c = c.start + (c.end - c.start + 1) / 2..c.end,
+            _ => panic!(),
+        });
+        found.push(8 * r.start + c.end - 1);
     }
     found.sort();
-    let mut curr = found[0];
-    for i in found {
-        if i - curr > 1 {
-            return i - 1;
-        }
-        curr = i;
-    }
-    unreachable!()
+    found
+        .iter()
+        .zip(found[0]..)
+        .find(|(&a, b)| a != *b)
+        .unwrap()
+        .1
 }
 
 fn main() {
