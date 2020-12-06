25a139eacaeaf843e91ca80ea0e514d27198b598
Wilco Kusee <wilcokusee@gmail.com>
2020-12-06 06:31:19 +0100

    Clean up day 6 using iterators

--- 2020/src/bin/day_6.rs
+++ 2020/src/bin/day_6.rs
@@ -1,46 +1,34 @@
+#![feature(iterator_fold_self)]
+
 use std::collections::HashSet;
 
 const INPUT: &str = include_str!("../../input/day_6.txt");
 
 fn part1() -> usize {
-    let mut result = 0;
-    for block in INPUT.split("\n\n") {
-        let mut yes = HashSet::new();
-
-        for line in block.lines() {
-            for c in line.chars() {
-                yes.insert(c);
-            }
-        }
-        result += yes.len()
-    }
-    result
+    INPUT
+        .split("\n\n")
+        .map(|block| {
+            block
+                .lines()
+                .flat_map(|line| line.chars())
+                .collect::<HashSet<_>>()
+                .len()
+        })
+        .sum()
 }
 
 fn part2() -> usize {
-    let mut result = 0;
-    for block in INPUT.split("\n\n") {
-        let mut yes: Option<HashSet<char>> = None;
-
-        for line in block.lines() {
-            let mut yes_line = HashSet::new();
-            for c in line.chars() {
-                yes_line.insert(c);
-            }
-            if yes.is_some() {
-                yes = Some(
-                    yes.unwrap()
-                        .intersection(&yes_line.into_iter().collect())
-                        .cloned()
-                        .collect(),
-                );
-            } else {
-                yes = Some(yes_line.into_iter().collect());
-            }
-        }
-        result += yes.unwrap().len()
-    }
-    result
+    INPUT
+        .split("\n\n")
+        .map(|block| {
+            block
+                .lines()
+                .map(|line| line.chars().collect::<HashSet<_>>())
+                .fold_first(|a, b| a.intersection(&b).cloned().collect())
+                .unwrap()
+                .len()
+        })
+        .sum()
 }
 fn main() {
     println!("Part 1: {}", part1());
