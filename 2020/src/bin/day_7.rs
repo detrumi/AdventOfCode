2324735d189aee74602f6d9cdc0fa8015baeb7a5
Wilco Kusee <wilcokusee@gmail.com>
2020-12-07 07:18:12 +0100

    Clean up solution

--- 2020/src/bin/day_7.rs
+++ 2020/src/bin/day_7.rs
@@ -1,114 +1,54 @@
 use std::collections::HashMap;
 
 const INPUT: &str = include_str!("../../input/day_7.txt");
+const SHINY_GOLD: &str = "shiny gold";
 
-type Color = Vec<String>;
-
-fn has_shiny_gold(color: Color, pairs: &HashMap<Color, Vec<Color>>) -> bool {
-    if pairs
-        .get(&color)
-        .unwrap()
-        .iter()
-        .any(|v| *v == shiny_gold())
-    {
-        return true;
-    }
-    for bag in pairs.get(&color).unwrap() {
-        if has_shiny_gold(bag.to_vec(), pairs) {
-            return true;
-        }
-    }
-    false
-}
-
-fn count_shiny_golds(color: Color, pairs: &HashMap<Color, Vec<(usize, Color)>>) -> usize {
-    pairs
-        .get(&color)
-        .unwrap()
-        .iter()
-        .map(|(amount, bag)| {
-            if *amount == 0 {
-                return 0;
-            }
-            amount + amount * count_shiny_golds(bag.clone(), pairs)
-        })
-        .sum()
-}
-
-fn shiny_gold() -> Color {
-    vec!["shiny".into(), "gold".into()]
-}
-
-fn part1() -> usize {
+fn parse() -> HashMap<String, Vec<(usize, String)>> {
     let mut pairs = HashMap::new();
     for line in INPUT.lines() {
         let parts: Vec<_> = line.split(" contain ").collect();
-        let color = parts[0]
-            .split_ascii_whitespace()
-            .map(|s| s.to_string())
-            .take(2)
-            .collect::<Color>();
-        let mut bags: Vec<Color> = parts[1]
-            .split(", ")
-            .map(|bag| {
-                if bag == "no other bags." {
-                    return vec![];
-                }
-                let parts = bag.split_ascii_whitespace().collect::<Vec<_>>();
-                let color = parts[1..3]
-                    .iter()
-                    .map(|s| s.to_string())
-                    .collect::<Vec<_>>();
-                color
-            })
-            .collect();
-        if bags[0].is_empty() {
-            bags = vec![];
-        }
-        pairs.insert(color, bags);
-    }
-
-    let mut result = 0;
-    for color in pairs.keys() {
-        if has_shiny_gold(color.clone(), &pairs) {
-            result += 1;
-        }
-    }
-
-    result
-}
-
-fn part2() -> usize {
-    let mut pairs = HashMap::new();
-    for line in INPUT.lines() {
-        let parts: Vec<_> = line.split(" contain ").collect();
-        let color = parts[0]
-            .split_ascii_whitespace()
-            .map(|s| s.to_string())
-            .take(2)
-            .collect::<Vec<_>>();
+        let color = parts[0].trim_end_matches(" bags").to_string();
         let bags: Vec<_> = parts[1]
             .split(", ")
             .map(|bag| {
                 if bag == "no other bags." {
-                    return (0, vec![]);
+                    return (0, String::new());
                 }
                 let parts = bag.split_ascii_whitespace().collect::<Vec<_>>();
-                let color = parts[1..3]
-                    .iter()
-                    .map(|s| s.to_string())
-                    .collect::<Vec<_>>();
-                let amount = parts
-                    .iter()
-                    .filter_map(|w| w.parse::<usize>().ok())
-                    .next()
-                    .unwrap();
+                let amount = parts[0].parse::<usize>().unwrap();
+                let color = parts[1..3].join(" ").to_string();
                 (amount, color)
             })
+            .filter(|(amount, _)| *amount > 0)
             .collect();
         pairs.insert(color, bags);
     }
-    count_shiny_golds(shiny_gold(), &pairs)
+    pairs
+}
+
+fn has_shiny_gold(color: String, pairs: &HashMap<String, Vec<(usize, String)>>) -> bool {
+    pairs[&color]
+        .iter()
+        .any(|(_, bag)| *bag == SHINY_GOLD || has_shiny_gold(bag.clone(), pairs))
+}
+
+fn count_shiny_golds(color: String, pairs: &HashMap<String, Vec<(usize, String)>>) -> usize {
+    pairs[&color]
+        .iter()
+        .map(|(amount, bag)| amount + amount * count_shiny_golds(bag.clone(), pairs))
+        .sum()
+}
+
+fn part1() -> usize {
+    let pairs = parse();
+    pairs
+        .keys()
+        .filter(|&color| has_shiny_gold(color.clone(), &pairs))
+        .count()
+}
+
+fn part2() -> usize {
+    count_shiny_golds(SHINY_GOLD.to_string(), &parse())
 }
 
 fn main() {
