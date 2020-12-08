ea95da50473e9f3476443cc3a70146502c4fba41
Wilco Kusee <wilcokusee@gmail.com>
2020-12-08 06:30:31 +0100

    Clean up solution

--- 2020/src/bin/day_8.rs
+++ 2020/src/bin/day_8.rs
@@ -2,73 +2,65 @@ use std::collections::HashSet;
 
 const INPUT: &str = include_str!("../../input/day_8.txt");
 
-fn part1() -> i32 {
-    let mut input = vec![];
-    for line in INPUT.lines() {
-        let parts: Vec<_> = line.split_ascii_whitespace().collect();
-        let instr = parts[0];
-        let num = parts[1].parse::<i32>().unwrap();
-        input.push((instr, num));
-    }
+#[derive(Clone, Copy, Debug)]
+enum Op {
+    Acc,
+    Jmp,
+    Nop,
+}
 
+fn parse() -> Vec<(Op, i32)> {
+    INPUT
+        .lines()
+        .map(|line| {
+            let parts: Vec<_> = line.split_ascii_whitespace().collect();
+            let instr = match parts[0] {
+                "acc" => Op::Acc,
+                "jmp" => Op::Jmp,
+                "nop" => Op::Nop,
+                _ => panic!(format!("Unknown op: {}", parts[0])),
+            };
+            let num = parts[1].parse::<i32>().unwrap();
+            (instr, num)
+        })
+        .collect()
+}
+
+fn solve(input: Vec<(Op, i32)>) -> Result<i32, i32> {
     let mut i = 0_i32;
     let mut acc = 0;
     let mut executed = HashSet::new();
-    loop {
-        let (instr, num) = input[i as usize];
-        if executed.contains(&i) {
-            break;
+    while executed.insert(i) {
+        if i as usize >= input.len() {
+            return Err(acc);
         }
-        executed.insert(i);
-        match instr {
-            "acc" => acc += num,
-            "jmp" => i += num - 1,
-            "nop" => (),
-            _ => panic!(),
+
+        let (op, num) = &input[i as usize];
+        match op {
+            Op::Acc => acc += num,
+            Op::Jmp => i += num - 1,
+            Op::Nop => (),
         }
         i += 1;
     }
+    Ok(acc)
+}
 
-    acc
+fn part1() -> i32 {
+    solve(parse()).unwrap()
 }
 
 fn part2() -> i32 {
-    let mut initial_input = vec![];
-    for line in INPUT.lines() {
-        let parts: Vec<_> = line.split_ascii_whitespace().collect();
-        let instr = parts[0];
-        let num = parts[1].parse::<i32>().unwrap();
-        initial_input.push((instr, num));
-    }
-
-    'outer: for (corrupt_index, (instr, _num)) in initial_input.iter().enumerate() {
+    let initial_input = parse();
+    for (i, (op, _num)) in initial_input.iter().enumerate() {
         let mut input = initial_input.clone();
-        match *instr {
-            "acc" => continue,
-            "jmp" => input[corrupt_index].0 = "nop",
-            "nop" => input[corrupt_index].0 = "jmp",
-            _ => panic!(),
+        match *op {
+            Op::Acc => continue,
+            Op::Jmp => input[i].0 = Op::Nop,
+            Op::Nop => input[i].0 = Op::Jmp,
         }
-
-        let mut i = 0_i32;
-        let mut acc = 0;
-        let mut executed = HashSet::new();
-        loop {
-            if i as usize >= input.len() {
-                return acc;
-            }
-            let (instr, num) = input[i as usize];
-            if executed.contains(&i) {
-                continue 'outer;
-            }
-            executed.insert(i);
-            match instr {
-                "acc" => acc += num,
-                "jmp" => i += num - 1,
-                "nop" => (),
-                _ => panic!(),
-            }
-            i += 1;
+        if let Err(acc) = solve(input) {
+            return acc;
         }
     }
     unreachable!()
