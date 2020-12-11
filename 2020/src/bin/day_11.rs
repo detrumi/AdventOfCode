307641111a311071963b4fdafc4abc4751176c44
Wilco Kusee <wilcokusee@gmail.com>
2020-12-11 06:55:23 +0100

    Deduplicate solutions

--- 2020/src/bin/day_11.rs
+++ 2020/src/bin/day_11.rs
@@ -4,118 +4,53 @@ fn parse() -> Vec<Vec<char>> {
     INPUT.lines().map(|line| line.chars().collect()).collect()
 }
 
-fn next(y: i32, x: i32, input: &Vec<Vec<char>>) -> char {
+fn next(y: i32, x: i32, input: &Vec<Vec<char>>, max_distance: i32, min_seats: usize) -> char {
     let mut occupied = 0;
-    for (dy, dx) in &[
-        (-1, -1),
-        (-1, 0),
-        (-1, 1),
-        (0, -1),
-        (0, 1),
-        (1, -1),
-        (1, 0),
-        (1, 1),
-    ] {
-        if y + dy >= 0
-            && y + dy < input.len() as i32
-            && x + dx >= 0
-            && x + dx < input[0].len() as i32
-        {
-            occupied += if input[(y + dy) as usize][(x + dx) as usize] == '#' {
-                1
-            } else {
-                0
-            };
+    for dy in -1..=1 {
+        for dx in (-1..=1).filter(|&dx| dy != 0 || dx != 0) {
+            for distance in 1..=max_distance {
+                match input
+                    .get((y + distance * dy) as usize)
+                    .and_then(|row| row.get((x + distance * dx) as usize))
+                {
+                    None => break,
+                    Some('#') => {
+                        occupied += 1;
+                        break;
+                    }
+                    Some('L') => break,
+                    _ => (),
+                }
+            }
         }
     }
-    let current = input[y as usize][x as usize];
-    match current {
+    match input[y as usize][x as usize] {
         'L' if occupied == 0 => '#',
-        '#' if occupied >= 4 => 'L',
-        _ => current,
+        '#' if occupied >= min_seats => 'L',
+        current => current,
     }
 }
 
-fn part1() -> usize {
-    let mut input = parse();
+fn solve(max_distance: i32, min_seats: usize) -> usize {
+    let mut grid = parse();
     for _round in 0.. {
-        let mut new = input.clone();
+        let mut new = grid.clone();
         for y in 0..new.len() {
             for x in 0..new[0].len() {
-                new[y][x] = next(y as i32, x as i32, &input)
+                new[y][x] = next(y as i32, x as i32, &grid, max_distance, min_seats)
             }
         }
-        if input == new {
+        if grid == new {
             break;
         }
-        input = new.clone();
+        grid = new.clone();
     }
-    input
-        .iter()
-        .map(|line| line.iter().filter(|&c| *c == '#').count())
-        .sum()
-}
-
-fn next_visible(y: i32, x: i32, input: &Vec<Vec<char>>) -> char {
-    let mut occupied = 0;
-    for (dy, dx) in &[
-        (-1, -1),
-        (-1, 0),
-        (-1, 1),
-        (0, -1),
-        (0, 1),
-        (1, -1),
-        (1, 0),
-        (1, 1),
-    ] {
-        for distance in 1.. {
-            let new_y = y + distance * dy;
-            let new_x = x + distance * dx;
-            if new_y < 0
-                || new_y >= input.len() as i32
-                || new_x < 0
-                || new_x >= input[0].len() as i32
-            {
-                break;
-            }
-
-            if input[(new_y) as usize][(new_x) as usize] == '#' {
-                occupied += 1;
-                break;
-            } else if input[(new_y) as usize][(new_x) as usize] == 'L' {
-                break;
-            }
-        }
-    }
-    let current = input[y as usize][x as usize];
-    match current {
-        'L' if occupied == 0 => '#',
-        '#' if occupied >= 5 => 'L',
-        _ => current,
-    }
-}
-
-fn part2() -> usize {
-    let mut input: Vec<Vec<_>> = parse();
-    for _round in 0.. {
-        let mut new = input.clone();
-        for y in 0..new.len() {
-            for x in 0..new[0].len() {
-                new[y][x] = next_visible(y as i32, x as i32, &input)
-            }
-        }
-        if input == new {
-            break;
-        }
-        input = new.clone();
-    }
-    input
-        .iter()
-        .map(|line| line.iter().filter(|&c| *c == '#').count())
-        .sum()
+    grid.iter()
+        .flat_map(|line| line.iter().filter(|&c| *c == '#'))
+        .count()
 }
 
 fn main() {
-    println!("Part 1: {}", part1());
-    println!("Part 2: {}", part2());
+    println!("Part 1: {}", solve(1, 4));
+    println!("Part 2: {}", solve(1000, 5));
 }
