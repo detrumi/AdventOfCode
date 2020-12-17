cf3b6a627c17e8e36ced62d7d6bc89de943f95c7
Wilco Kusee <wilcokusee@gmail.com>
2020-12-17 13:09:39 +0100

    Improve performance

--- 2020/src/bin/day_17.rs
+++ 2020/src/bin/day_17.rs
@@ -1,12 +1,12 @@
 use itertools::Itertools;
 
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::iter;
 
 const INPUT: &str = include_str!("../../input/day_17.txt");
 
-fn solve(dimensions: usize) -> usize {
-    let state: HashSet<Vec<i32>> = INPUT
+fn parse(dimensions: usize) -> HashSet<Vec<i32>> {
+    INPUT
         .lines()
         .enumerate()
         .flat_map(|(y, line)| {
@@ -19,30 +19,29 @@ fn solve(dimensions: usize) -> usize {
                     v
                 })
         })
-        .collect();
+        .collect()
+}
 
+fn solve(dimensions: usize) -> usize {
     (1..=6)
-        .fold(state, |state, cycle| {
-            iter::repeat(-cycle..INPUT.lines().count() as i32 + cycle)
-                .take(dimensions)
-                .multi_cartesian_product()
-                .filter(|positions| {
-                    let count = iter::repeat(-1..=1)
-                        .take(dimensions)
-                        .multi_cartesian_product()
-                        .filter(|deltas| deltas.iter().any(|d| *d != 0))
-                        .filter(|deltas| {
-                            state.contains(
-                                &positions
-                                    .iter()
-                                    .zip(deltas)
-                                    .map(|(p, d)| p + d)
-                                    .collect::<Vec<i32>>(),
-                            )
-                        })
-                        .count();
-                    count == 3 || (count == 2 && state.contains(positions))
+        .fold(parse(dimensions), |state, _cycle| {
+            let mut counts: HashMap<Vec<i32>, usize> = HashMap::new();
+            for position in state.iter() {
+                for neighbor in iter::repeat(-1..=1)
+                    .take(dimensions)
+                    .multi_cartesian_product()
+                    .filter(|delta| delta.iter().any(|d| *d != 0))
+                    .map(|delta| position.iter().zip(delta).map(|(p, d)| p + d).collect())
+                {
+                    *counts.entry(neighbor).or_insert(0) += 1;
+                }
+            }
+            counts
+                .into_iter()
+                .filter(|(position, count)| {
+                    *count == 3 || (*count == 2 && state.contains(position))
                 })
+                .map(|t| t.0)
                 .collect()
         })
         .len()
