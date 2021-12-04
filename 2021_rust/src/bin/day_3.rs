6486fbf52b6c8ff574aa376a95d00d58cc1944f7
Wilco Kusee <wilcokusee@gmail.com>
2021-12-04 07:21:25 +0100

    Clean up day 3

--- 2021_rust/src/bin/day_3.rs
+++ 2021_rust/src/bin/day_3.rs
@@ -1,83 +1,51 @@
 const INPUT: &str = include_str!("../../input/day_3.txt");
 
-fn dec(v: &[char]) -> usize {
-    let mut result = 0;
-    for i in 0..v.len() {
-        if v[i] == '1' {
-            result += 2_usize.pow((v.len() - i - 1) as u32);
-        }
-    }
-    result
+fn parse() -> Vec<Vec<char>> {
+    INPUT
+        .trim()
+        .lines()
+        .map(|line| line.chars().collect())
+        .collect()
+}
+
+fn to_decimal(s: String) -> usize {
+    usize::from_str_radix(&s, 2).unwrap()
+}
+
+fn bool_to_bit(b: bool) -> char {
+    char::from_digit(b as u32, 10).unwrap()
+}
+
+fn count(lines: &[Vec<char>], index: usize) -> usize {
+    lines.iter().filter(|v| v[index] == '1').count()
 }
 
 fn part1() -> usize {
-    let lines: Vec<_> = INPUT.trim().lines().collect();
-    let mut counts = vec![0; lines[0].len()];
-    for i in 0..lines.len() {
-        let chars: Vec<_> = lines[i].chars().collect();
-        for j in 0..lines[0].len() {
-            if chars[j] == '1' {
-                counts[j] += 1;
-            }
-        }
-    }
-    let mut gamma = 0;
-    for i in 0..counts.len() {
-        if counts[i] >= lines.len() / 2 {
-            gamma += 1 << (counts.len() - i - 1);
-        }
-    }
-    let epsilon = gamma ^ ((1 << counts.len()) - 1);
+    let lines = parse();
+    let gamma: String = (0..lines[0].len())
+        .map(|i| bool_to_bit(2 * count(&lines, i) >= lines.len()))
+        .collect();
+    let gamma = to_decimal(gamma);
+    let epsilon = gamma ^ ((1 << lines[0].len()) - 1);
     gamma * epsilon
 }
 
 fn part2() -> usize {
-    let lines: Vec<_> = INPUT
-        .trim()
-        .lines()
-        .map(|line| line.chars().collect::<Vec<_>>())
-        .collect();
+    let lines = parse();
     let mut oxygen = lines.clone();
     let mut scrubber = lines.clone();
+
     for n in 0..lines[0].len() {
-        let mut counts_o = vec![0; oxygen[0].len()];
-        for i in 0..oxygen.len() {
-            for j in 0..oxygen[0].len() {
-                if oxygen[i][j] == '1' {
-                    counts_o[j] += 1;
-                }
-            }
-        }
-
-        let mut counts_s = vec![0; scrubber[0].len()];
-        for i in 0..scrubber.len() {
-            for j in 0..scrubber[0].len() {
-                if scrubber[i][j] == '1' {
-                    counts_s[j] += 1;
-                }
-            }
-        }
-
         if oxygen.len() > 1 {
-            let old = oxygen.clone();
-            oxygen.retain(|v| {
-                if 2 * counts_o[n] < old.len() {
-                    return v[n] == '0';
-                };
-                return v[n] == '1';
-            });
+            let bit = bool_to_bit(2 * count(&oxygen, n) >= oxygen.len());
+            oxygen.retain(|v| v[n] == bit);
         }
         if scrubber.len() > 1 {
-            let old = scrubber.clone();
-            scrubber.retain(|v| {
-                if 2 * counts_s[n] < old.len() {
-                    return v[n] == '1';
-                };
-                return v[n] == '0';
-            });
+            let bit = bool_to_bit(2 * count(&scrubber, n) < scrubber.len());
+            scrubber.retain(|v| v[n] == bit);
         }
     }
-    dec(&oxygen[0]) * dec(&scrubber[0])
+    to_decimal(oxygen[0].iter().collect()) * to_decimal(scrubber[0].iter().collect())
 }
 
 fn main() {
