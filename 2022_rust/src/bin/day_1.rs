ef83fecd05f30e8b9fc9a10583b37d857bb02cfb
Wilco Kusee <wilcokusee@gmail.com>
2022-12-01 06:19:40 +0100

    2022 day 1

---
+++ 2022_rust/src/bin/day_1.rs
@@ -0,0 +1,33 @@
+use itertools::Itertools;
+
+const INPUT: &str = include_str!("../input/day_1.txt");
+
+fn parse() -> impl Iterator<Item = Vec<usize>> {
+    INPUT.split("\n\n").map(|group| {
+        group
+            .lines()
+            .map(|line| line.parse::<usize>().unwrap())
+            .collect()
+    })
+}
+
+fn part1() -> usize {
+    parse()
+        .map(|group| group.iter().sum::<usize>())
+        .max()
+        .unwrap()
+}
+
+fn part2() -> usize {
+    parse()
+        .map(|group| group.iter().sum::<usize>())
+        .sorted()
+        .rev()
+        .take(3)
+        .sum()
+}
+
+fn main() {
+    println!("Part 1: {}", part1());
+    println!("Part 2: {}", part2());
+}
---
+++ 2022_rust/src/bin/template.rs
@@ -0,0 +1,20 @@
+const INPUT: &str = include_str!("../input/day_1.txt");
+
+fn parse() -> Vec<String> {
+    INPUT.lines().map(|line| line.parse().unwrap()).collect()
+}
+
+fn part1() -> usize {
+    parse();
+    0
+}
+
+fn part2() -> usize {
+    parse();
+    0
+}
+
+fn main() {
+    println!("Part 1: {}", part1());
+    println!("Part 2: {}", part2());
+}
