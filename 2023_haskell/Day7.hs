ca9c21e17a7f64d0ef6b5f8a8fbabd9522443d53
Wilco Kusee <wilcokusee@gmail.com>
2023-12-07 19:26:36 +0100

    Further shorten day 7

--- 2023_haskell/Day7.hs
+++ 2023_haskell/Day7.hs
@@ -1,4 +1,4 @@
-import Data.List (group, sortBy, elemIndex, sortBy, transpose)
+import Data.List (group, elemIndex, sort, sortBy, transpose)
 import Data.Function (on)
 import Data.Ord (comparing, Down (Down))
 
@@ -6,7 +6,6 @@ main = do [hands, bids] <- transpose . map words . lines <$> readFile "input/day
           print . solve "AKQJT98765432" id id $ zip hands bids
           print . solve "AKQT98765432J" (filter (/= 'J')) ((\(_:ls) -> 5-sum ls:ls) . (++ [0])) $ zip hands bids
 
-solve s f g = let handType = g . sortBy (comparing Down) . map length . group . sortBy compareStrength . f
-                  compareStrength = comparing (`elemIndex` s)
-              in  sum . zipWith (*) [1..] . map (read . snd) . sortBy ((\a b -> mconcat $ comparing handType a b : zipWith compareStrength b a) `on` fst)
+solve s f g = let handType = g . sortBy (comparing Down) . map length . group . sort . f
+              in  sum . zipWith (*) [1..] . map (read . snd) . sortBy ((\a b -> mconcat $ comparing handType a b : zipWith (comparing (`elemIndex` s)) b a) `on` fst)
 
