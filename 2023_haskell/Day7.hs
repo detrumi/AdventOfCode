5235e3186c35fd5627ae6ed84faa531a39903658
Wilco Kusee <wilcokusee@gmail.com>
2023-12-07 16:59:03 +0100

    2023 day 7 part 2

--- 2023_haskell/Day7.hs
+++ 2023_haskell/Day7.hs
@@ -5,9 +5,16 @@ import Data.Maybe (fromJust)
 main = do
     [hands, bids] <- transpose . map words . lines <$> readFile "input/day_7.txt"
     print $ part1 $ zip hands bids
-    -- print $ part2 input
+    print $ part2 $ zip hands bids
 
 part1 = sum . zipWith (*) [1..] . map (read . snd) . sortBy ((\a b -> (compare `on` handType) b a <> secondOrdering b a) `on` fst)
 secondOrdering a b = head . filter (/= EQ) $ zipWith compareStrength a b
 handType = fromJust . (`elemIndex` [[5],[1,4],[2,3],[1,1,3],[1,2,2],[1,1,1,2],[1,1,1,1,1]]) . sort . map length . group . sortBy compareStrength
 compareStrength = compare `on` flip elemIndex "AKQJT98765432"
+
+part2 = sum . zipWith (*) [1..] . map (read . snd) . sortBy ((\a b -> (compare `on` handType') b a <> secondOrdering' b a) `on` fst)
+secondOrdering' a b = head . filter (/= EQ) $ zipWith compareStrength' a b
+handType' hand = fromJust . (`elemIndex` [[5],[1,4],[2,3],[1,1,3],[1,2,2],[1,1,1,2],[1,1,1,1,1]]) . (\l -> if not (null l) then init l ++ [last l + n] else [5])
+    . sort . map length . group . sortBy compareStrength' $ filter (/= 'J') hand
+    where n = length $ filter (== 'J') hand
+compareStrength' = compare `on` flip elemIndex "AKQT98765432J"
