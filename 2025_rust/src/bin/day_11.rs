bee9a87ae39723295b405eb50e2e1ec27eb7a5fb
Wilco Kusee <wilcokusee@gmail.com>
2025-12-12 10:01:07 +0100

    2025 day 11 part 2

--- 2025_rust/src/bin/day_11.rs
+++ 2025_rust/src/bin/day_11.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use itertools::Itertools;
 
 const INPUT: &str = include_str!("../../input/day_11.txt");
@@ -39,73 +41,84 @@ fn part1() -> usize {
     result
 }
 
-// fn part2() -> usize {
-//     // let mut result = 0;
-//     // let parsed = parse().into_iter().collect::<FxHashMap<_, _>>();
-//     // let mut devices = vec!["svr".to_string()];
-//     // while !devices.is_empty() {
-//     //     println!("{}", devices.len());
-//     //     let mut new_devices = vec![];
-//     //     for input in devices {
-//     //         let outputs = &parsed[&input];
-//     //         for output in outputs {
-//     //             if output == "out" {
-//     //                 result += 1;
-//     //             } else {
-//     //                 new_devices.push(output.clone());
-//     //             }
-//     //         }
-//     //     }
-//     //     devices = new_devices.into_iter().collect();
-//     // }
-//     // result
+#[derive(Default, Debug)]
+struct PathCount {
+    neither: usize,
+    dac: usize,
+    fft: usize,
+    both: usize,
+}
 
-//     let parsed = parse().into_iter().collect::<FxHashMap<_, _>>();
-//     solve(&parsed, &"svr".to_string())
-// }
-
-// fn solve(parsed: &FxHashMap<String, Vec<String>>, input: &String) -> usize {
-//     let mut result = 0;
-//     let outputs = &parsed[input];
-//     for output in outputs {
-//         if output == "out" {
-//             result += 1;
-//         } else {
-//             result += solve(parsed, &output);
-//             eprintln!("result = {:?}", result);
-//         }
-//     }
-//     result
-// }
+/*
+Algo:
+- Start from svr
+- Only process nodes that have all incoming paths calculated
+- Track counts for all combinations
+*/
 
 fn part2() -> usize {
-    let mut result = 0;
+    let mut inputs: HashMap<String, Vec<String>> = HashMap::new();
+    let mut devices = vec![];
     let parsed = parse();
-    let mut devices = vec![("out".to_string(), false, false)];
-    // let paths = FxHashMap::new();
-    while !devices.is_empty() {
-        eprintln!("devices.len() = {:?}", devices.len());
-        let mut new_devices = vec![];
-        for (target, dac, fft) in devices {
-            if target == "svr" && dac && fft {
-                result += 1;
-            }
-            for (device, outputs) in &parsed {
-                if outputs.contains(&target) {
-                    new_devices.push((
-                        device.clone(),
-                        dac || device == "dac",
-                        fft || device == "fft",
-                    ));
+    for (device, outputs) in parsed {
+        if device != "svr" {
+            devices.push(device.clone());
+        }
+        for output in outputs {
+            inputs.entry(output).or_default().push(device.clone());
+        }
+    }
+
+    let mut counts = HashMap::new();
+    counts.insert(
+        "svr".to_string(),
+        PathCount {
+            neither: 1,
+            dac: 0,
+            fft: 0,
+            both: 0,
+        },
+    );
+
+    let mut progressing = true;
+    while progressing {
+        progressing = false;
+        for device_ix in (0..devices.len()).rev() {
+            if inputs[&devices[device_ix]]
+                .iter()
+                .all(|input| counts.contains_key(input))
+            {
+                progressing = true;
+                let device = &devices[device_ix];
+                let mut count = PathCount::default();
+                for input in &inputs[device] {
+                    count.neither += counts[input].neither;
+                    count.dac += counts[input].dac;
+                    count.fft += counts[input].fft;
+                    count.both += counts[input].both;
                 }
+                if device == "dac" {
+                    count.dac += count.neither;
+                    count.neither = 0;
+                    count.both += count.fft;
+                    count.fft = 0;
+                } else if device == "fft" {
+                    count.fft += count.neither;
+                    count.neither = 0;
+                    count.both += count.dac;
+                    count.dac = 0;
+                }
+
+                counts.insert(device.clone(), count);
+                devices.remove(device_ix);
             }
         }
-        devices = new_devices;
     }
-    result
+
+    inputs["out"].iter().map(|device| counts[device].both).sum()
 }
 
 fn main() {
-    println!("Part 1: {}", part1()); // 37, 21 wrong
+    println!("Part 1: {}", part1());
     println!("Part 2: {}", part2());
 }
